8d81238087633a3c867c23a95ede13925997ed88
Jose Luis Lopez Bellido <joseluislobell@gmail.com>
2021-10-28 20:58:16 +0200

    Create index with chakra-ui components

--- texts/texts.js
+++ texts/texts.js
@@ -1,7 +1,27 @@
 export const languajes = ["en","es"]
 export const texts = {
-    greetings: {
-        en: "HELLO",
-        es: "HOLA"
+    index: {
+        greetings: {
+            en: "Techy Blog",
+            es: "Blog de Tecnología"
+        },
+        description: {
+            en: "Welcome to JLLB.io, this is a project for a technology blog",
+            es: "Bienvenid@s a JLLB.io, este es un proyecto para crear un blog de tecnología"
+        },
+        aboutme: {
+            en: "I am a Software engineer from Madrid SPAIN, I am a Software engineer from Madrid SPAIN, I am a Software engineer from Madrid SPAIN , I am a Software engineer from Madrid SPAIN, I am a Software engineer from Madrid SPAIN, I am a Software engineer from Madrid SPAIN, I am a Software engineer from Madrid SPAIN",
+            es: "Soy un ingeniero de software de Madrid, Soy un ingeniero de software de Madrid, Soy un ingeniero de software de Madrid, Soy un ingeniero de software de Madrid, Soy un ingeniero de software de Madrid, Soy un ingeniero de software de Madrid"
+        }
+    },
+    name: "Jose Luis López Bellido",
+    email: "joseluislobell@gmail.com",
+    copyright: {
+        en: "All rights reserved",
+        es: "Todos los derechos reservados"
+    },
+    links: {
+        linkedin: "https://www.linkedin.com/in/jose-luis-lopez-bellido/",
+        github: "https://github.com/PepeluDev"
     }
 }
